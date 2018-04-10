485e56f64524623eba9487b8b0ea7b52851dffcd
cgoez <goez2001@gmail.com>
2018-04-10 14:19:40 -0700

    NavWrapper outline

---
+++ apple-nav/src/Components/NavWrapper.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Nav from './Nav.js';
+import SubNav from './SubNav';
+
+const NavWrapper = props => {
+    return (
+        <div>
+        Mac
+        </div>,
+        <div>
+        iPod
+        </div>,
+        <div>
+        iPhone
+        </div>,
+        <div>
+        Watch
+        </div>,
+        <div>
+        TV
+        </div>,
+        <div>
+        Music
+        </div>,
+        <div>
+        Support 
+        </div>
+    )
+}
+
+export default NavWrapper;
