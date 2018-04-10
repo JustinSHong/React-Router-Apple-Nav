54e22ca9737449f2549a0d2d976a27d78d87806c
Justin Hong <jhong2009@gmail.com>
2018-04-10 15:24:42 -0700

    set up general layout of Navwrapper

--- apple-nav/src/Components/NavWrapper.js
+++ apple-nav/src/Components/NavWrapper.js
@@ -1,39 +1,43 @@
 import React from 'react';
-import ChildNav from './childNav.js';
-import SubNav from './SubNav';
 import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
 
+import ChildNav from './childNav.js';
+import SubNav from './SubNav';
+import "./NavWrapper.css";
+
+// renders the main navigation bar
 const NavWrapper = props => {
-    return (
-        <div>
-        <Navbar color="faded" light>
-          <NavbarBrand href="/" className="mr-auto"></NavbarBrand>
-            <Nav navbar>
-              <NavItem>
-                <NavLink href="/components/">Mac</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="">iPod</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="/components/">iPhone</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="/components/">Watch</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="/components/">TV</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="/components/">Music</NavLink>
-              </NavItem>
-              <NavItem>
-              <NavLink href="/components/">Support</NavLink>
-            </NavItem>
-            </Nav>
-        </Navbar>
+  return (
+    <div className="NavWrapper">
+      <Navbar className="Navbar" color="faded" light>
+        {/* apple icon */}
+        <NavbarBrand href="/" className="logo">AppleLogo</NavbarBrand>
+        <Nav className="Nav">
+          <NavItem>
+            <NavLink href="/components/">Mac</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="">iPod</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="/components/">iPhone</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="/components/">Watch</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="/components/">TV</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="/components/">Music</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="/components/">Support</NavLink>
+          </NavItem>
+        </Nav>
+       </Navbar>
       </div>
-);
+  );
 };
 
 export default NavWrapper;
